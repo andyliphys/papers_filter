a05bfe1a70941045d4d05b633fd28a303dfb97da
Andy C. Y. Li <andyliphys@gmail.com>
2017-05-24 20:54:15 -0500

    v1.01 Small optimization.

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 # papers_filter
 
+v1.01 (24-May-2017)
+Update the arXiv condensed-matter input source to the whole catagory instead of the nano-device sub-catagory.
+Change the filtering to single-thread from multi-thread.
+
 v1.0 (22-May-2017)
 Filtering for arXiv and Nature papers.
--- filter.m
+++ filter.m
@@ -2,7 +2,7 @@
 targetPath="/home/ali/Dropbox/Server/papers/";
 triggerDay=1;(*Keep the nature output for the defined number of days between current and publication date*)
 
-authorsList={"Blais","Glazman","Girvin","Schoelkopf","Catelani","DiCarlo","Houck","Schuster","Le Hur","Delsing","Wallraff","Gambetta","Korotkov","Petruccione","Devoret","Manucharyan","Hekking","Guichard","Kerman","Koch","Lukin","Prosen","Cirac","Keeling","Hartmann","Schmidt","Hornberger","Nori","Tureci","Kehrein","Hafezi","Zwolak","T. E. Lee" ,"Knap","Martinis","Jiasen Jin","L. Jiang","Ciuti", "Mitra", "Wilhelm", "Lupascu", "C.M. Wilson", "C. M. Wilson", "Jerry M. Chow", "Solano", "J. Q. You", "A. A. Clerk", "Aashish A. Clerk", "DiVincenzo", "Burkard"};
+authorsList={"Blais","Glazman","Girvin","Schoelkopf","Catelani","DiCarlo","Houck","Schuster","Le Hur","Delsing","Wallraff","Gambetta","Korotkov","Petruccione","Devoret","Manucharyan","Hekking","Guichard","Kerman","Koch","Lukin","Prosen","Cirac","Keeling","Hartmann","Schmidt","Hornberger","Nori","Tureci","Kehrein","Hafezi","Zwolak","T. E. Lee","Knap","Martinis","Jiasen Jin","L. Jiang","Ciuti","Mitra","Wilhelm","Lupascu","C.M. Wilson","C. M. Wilson","Jerry M. Chow","Solano","J. Q. You","A. A. Clerk","Aashish A. Clerk","DiVincenzo","Burkard"};
 keywordsList={"circuit QED","artificial atom","superconducting qubit","superconducting circuits","quantum circuits","Jaynes-Cummings","Bose-Hubbard","non-equilibrium phase","nonequilibrium phase","dissipative phase","dynamical phase","non-equilibrium steady state","nonequilibrium steady state","NESS", "non-equilibrium statistical mechanics","nonequilibrium statistical mechanics","open quantum systems" ,"open systems","matrix product operators","Matrix Product states","Keldysh","Dissipative Many-Body","non-Hermitian","Floquet","photon condensate","driven-dissipative","quantum simulators","quantum simulation"};
 
 now=DateList[];
@@ -48,7 +48,7 @@ filterf[objectInput_]:=Block[{objectGo,objectGoLen,keepQList,KeepList},
 objectGo=objectInput;
 objectGoLen=Length[objectGo];
 
-keepQList=ParallelTable[AnyTrue[Join[Table[StringContainsQ[ToString[objectGo[[itemid]]],authorsList[[j]]],{j,1,authorsListLen}],Table[StringContainsQ[ToString[objectGo[[itemid]]],keywordsList[[j]]],{j,1,keywordsListLen}]],TrueQ],{itemid,1,objectGoLen}];
+keepQList=Table[AnyTrue[Join[Table[StringContainsQ[ToString[objectGo[[itemid]]],authorsList[[j]]],{j,1,authorsListLen}],Table[StringContainsQ[ToString[objectGo[[itemid]]],keywordsList[[j]]],{j,1,keywordsListLen}]],TrueQ],{itemid,1,objectGoLen}];
 
 KeepList={};
 Do[If[keepQList[[itemid]],AppendTo[KeepList,itemid]],{itemid,1,objectGoLen}];
@@ -73,7 +73,8 @@ link="<p class=\"Text\">"<>"<a href=\""<>item[[2]]<> "\" target=\"_blank\">"<>it
 Return[title<>main<>link<>"<hr>"]
 ];
 arXivQPhy=Import["http://export.arxiv.org/rss/quant-ph?version=2.0","XML"];
-arXivNano=Import["http://export.arxiv.org/rss/cond-mat.mes-hall?version=2.0","XML"];
+(*arXivNano=Import["http://export.arxiv.org/rss/cond-mat.mes-hall?version=2.0","XML"];*)
+arXivNano=Import["http://export.arxiv.org/rss/cond-mat?version=2.0","XML"];
 arXivList={arXivQPhy,arXivNano};arXivListLen=Length[arXivList];
 arXivList=Union[filterf[Flatten[Table[extractf[arXivList[[j]],"arXiv"],{j,1,arXivListLen}],1]]];
 
